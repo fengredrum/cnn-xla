4173cca920a3b8479ef93ae3e6edb6b011f089a4
Oblivion <fengredrum@163.com>
2020-06-13 23:09:47 +0800

    Add an activation selecting function

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
-from .activations import Swish, Mish
+# from .activations import Swish, Mish
 
-from .alexnet import alexnet
-from .vgg import vgg11
-from .resnet import resnet18
-from .densenet import densenet18
+# from .alexnet import alexnet
+# from .vgg import vgg11
+# from .resnet import resnet18
+# from .densenet import densenet18
--- models/activations.py
+++ models/activations.py
@@ -3,6 +3,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def activetion_func(activation_name):
+    if activation_name == 'relu':
+        activation = nn.ReLU()
+    elif activation_name == 'tanh':
+        activation = nn.Tanh()
+    elif activation_name == 'mish':
+        activation = Mish()
+    elif activation_name == 'swish':
+        activation = Swish()
+    else:
+        raise NotImplementedError
+    return activation
+
+
 class Mish(nn.Module):
     def __init__(self):
         super(Mish, self).__init__()
