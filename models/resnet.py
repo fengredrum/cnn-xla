00d9c7a846ed504d5cdea9f1d3179e5d5a45955b
Oblivion <fengredrum@163.com>
2020-06-15 12:25:24 +0800

    Modify the implementation of ResNet

--- models/resnet.py
+++ models/resnet.py
@@ -10,9 +10,14 @@ import torch.nn.functional as F
 from .activations import activetion_func
 
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, activation='relu'):
-        super(ResidualBlock, self).__init__()
+class BasicBlock(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride=1,
+                 expansion=1,
+                 activation='relu'):
+        super(BasicBlock, self).__init__()
         self.activation = activetion_func(activation)
 
         self.trunk = nn.Sequential(
@@ -21,42 +26,85 @@ class ResidualBlock(nn.Module):
                       kernel_size=3,
                       padding=1,
                       stride=stride,
-                      bias=False),
-            nn.BatchNorm2d(out_channels),
+                      bias=False), nn.BatchNorm2d(out_channels),
             self.activation,
             nn.Conv2d(out_channels,
                       out_channels,
                       kernel_size=3,
                       padding=1,
                       stride=1,
-                      bias=False),
-        )
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
+                      bias=False))
+
+        if stride != 1 or in_channels != expansion * out_channels:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_channels,
-                          out_channels,
+                          expansion * out_channels,
                           kernel_size=1,
                           stride=stride,
                           bias=False),
-                nn.BatchNorm2d(out_channels),
-            )
+                nn.BatchNorm2d(expansion * out_channels))
+        else:
+            self.shortcut = nn.Sequential()
 
     def forward(self, x):
         out = self.trunk(x)
         out += self.shortcut(x)
-        return torch.relu(out)
+        return self.activation(out)
+
+
+class BottleneckBlock(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride=1,
+                 expansion=4,
+                 activation='relu'):
+        super(BottleneckBlock, self).__init__()
+        self.activation = activetion_func(activation)
+
+        self.trunk = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels), self.activation,
+            nn.Conv2d(out_channels,
+                      out_channels,
+                      kernel_size=3,
+                      padding=1,
+                      stride=stride,
+                      bias=False), nn.BatchNorm2d(out_channels),
+            self.activation,
+            nn.Conv2d(out_channels,
+                      expansion * out_channels,
+                      kernel_size=1,
+                      bias=False), nn.BatchNorm2d(expansion * out_channels))
+
+        if stride != 1 or in_channels != expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels,
+                          expansion * out_channels,
+                          kernel_size=1,
+                          stride=stride,
+                          bias=False),
+                nn.BatchNorm2d(expansion * out_channels))
+        else:
+            self.shortcut = nn.Sequential()
+
+    def forward(self, x):
+        out = self.trunk(x)
+        out += self.shortcut(x)
+        return self.activation(out)
 
 
 class ResNet(nn.Module):
     def __init__(self,
                  residual_block,
                  num_blocks,
+                 expansion=1,
                  activation='relu',
                  num_classes=10):
         super(ResNet, self).__init__()
         assert len(num_blocks) == 4, 'Invalid Conv Number!'
         self.activation = activetion_func(activation)
+        self.expansion = expansion
 
         self.num_channels = 64
         self.conv1 = nn.Conv2d(3,
@@ -69,31 +117,37 @@ class ResNet(nn.Module):
         self.layer1 = self._make_layer(residual_block,
                                        self.num_channels,
                                        num_blocks[0],
-                                       stride=1)
+                                       stride=1,
+                                       activation=activation)
         self.layer2 = self._make_layer(residual_block,
                                        128,
                                        num_blocks[1],
-                                       stride=2)
+                                       stride=2,
+                                       activation=activation)
         self.layer3 = self._make_layer(residual_block,
                                        256,
                                        num_blocks[2],
-                                       stride=2)
+                                       stride=2,
+                                       activation=activation)
         self.layer4 = self._make_layer(residual_block,
                                        512,
                                        num_blocks[3],
-                                       stride=2)
-        self.linear = nn.Linear(512, num_classes)
+                                       stride=2,
+                                       activation=activation)
+        self.linear = nn.Linear(512 * self.expansion, num_classes)
 
-    def _make_layer(self, block, out_channels, num_blocks, stride):
+    def _make_layer(self, block, out_channels, num_blocks, stride, activation):
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
-            layers.append(block(self.num_channels, out_channels, stride))
-            self.num_channels = out_channels
+            layers.append(
+                block(self.num_channels, out_channels, stride, self.expansion,
+                      activation))
+            self.num_channels = out_channels * self.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.activation(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
@@ -105,7 +159,36 @@ class ResNet(nn.Module):
 
 
 def resnet18(activation='relu', num_classes=10):
-    return ResNet(ResidualBlock, [2, 2, 2, 2],
+    return ResNet(BasicBlock, [2, 2, 2, 2],
+                  expansion=1,
+                  activation=activation,
+                  num_classes=num_classes)
+
+
+def resnet34(activation='relu', num_classes=10):
+    return ResNet(BasicBlock, [3, 4, 6, 3],
+                  expansion=1,
+                  activation=activation,
+                  num_classes=num_classes)
+
+
+def resnet50(activation='relu', num_classes=10):
+    return ResNet(BottleneckBlock, [3, 4, 6, 3],
+                  expansion=4,
+                  activation=activation,
+                  num_classes=num_classes)
+
+
+def resnet101(activation='relu', num_classes=10):
+    return ResNet(BottleneckBlock, [3, 4, 23, 3],
+                  expansion=4,
+                  activation=activation,
+                  num_classes=num_classes)
+
+
+def resnet152(activation='relu', num_classes=10):
+    return ResNet(BottleneckBlock, [3, 8, 36, 3],
+                  expansion=4,
                   activation=activation,
                   num_classes=num_classes)
 
